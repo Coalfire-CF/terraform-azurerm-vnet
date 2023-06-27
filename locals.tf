4e603f9df7abf3e127b827c8a3b055476729dca9
Douglas Francis <douglas@poshops.io>
2023-06-27 11:02:49 -0500

    pulled vnet module from MK repo

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
-locals {
-  azurerm_subnets = {
-    for index, subnet in azurerm_subnet.main :
-    subnet.name => subnet.id
-  }
-}
+# locals {
+#   azurerm_subnets = {
+#     for index, subnet in azurerm_subnet.main :
+#     subnet.name => subnet.id
+#   }
+# }
