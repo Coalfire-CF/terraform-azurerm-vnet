6475bf0f973d6f93e1a22f3f10180c69dc89dc90
Herman Wong <herman.wong@coalfire.com>
2025-02-05 11:32:32 -0800

    Pin module to tag (tflint, sbom script)

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "default" {
 }
 
 module "diag" {
-  source                = "github.com/Coalfire-CF/terraform-azurerm-diagnostics"
+  source                = "github.com/Coalfire-CF/terraform-azurerm-diagnostics?ref=v1.0.0"
   diag_log_analytics_id = var.diag_log_analytics_id
   resource_id           = azurerm_virtual_network.vnet.id
   resource_type         = "vnet"
